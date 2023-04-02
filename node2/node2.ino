9fc7675c74292af85fb1d16f33523575af762b70
Niumandzi <amazheykin@gmail.com>
2023-04-02 23:35:35 +0300

    need fix

--- node2/node2.ino
+++ node2/node2.ino
@@ -1,64 +1,93 @@
-#include "painlessMesh.h"
-#define MESH_PREFIX "whateverYouLike"
-#define MESH_PASSWORD "somethingSneaky"
+#include <painlessMesh.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+
+#define MESH_PREFIX "Greenhouse by Niumandzi"
+#define MESH_PASSWORD "Password"
 #define MESH_PORT 5555
 
-Scheduler userScheduler; // для контроля
+#define DHTPIN 3 
+#define DHTTYPE DHT11 
+
+#define SOILPIN A0
+
+Scheduler userScheduler;
 painlessMesh mesh;
-String messageToSend = "";
 
-void sendMessage() ; // чтобы PlatformIO работал
+struct Node2DataToSend {
+  int soilMoisture;
+  float airTemperature;
+  float airHumidity;
+};
 
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+Node2DataToSend data;
+DHT dht(DHTPIN, DHTTYPE);
 
 void sendMessage() {
-  if(messageToSend.length() > 0) {
-  String msg = "Node " + String(mesh.getNodeId()) + " says: " + messageToSend;
-  mesh.sendBroadcast(msg);
-  Serial.println("Sent message: " + msg);
-  messageToSend = "";
-}
-  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+  DynamicJsonDocument node2(1024);
+  node2["soilMoisture2"] = data.soilMoisture;
+  node2["airTemperature2"] = data.airTemperature;
+  node2["airHumidity2"] = data.airHumidity;
+
+  String json;
+  serializeJson(node2, json);
+  mesh.sendBroadcast(json);
+
+  Serial.println("Sent message");
 }
 
-void receivedCallback( uint32_t from, String &msg ) {
+void readSoilMoisture() {
+  data.soilMoisture = analogRead(SOILPIN);
+  Serial.println(analogRead(SOILPIN));
+}
+
+void readAirIndicators() {
+  data.airTemperature = dht.readTemperature(); 
+  data.airHumidity = dht.readHumidity(); 
+  Serial.println(dht.readTemperature(), dht.readHumidity());
+}
+
+void readTensorIndicator() {
+  int tensor = analogRead(A0);
+  Serial.println(tensor);
+}
+
+void writeWindowControl() {
+
+}
+
+void receivedCallback(uint32_t from, String &msg) {
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+}
 
-  if (msg.indexOf("test") >= 0) {
-    Serial.println("test word was detected");
-}
-}
 void newConnectionCallback(uint32_t nodeId) {
   Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
+
 void changedConnectionCallback() {
   Serial.printf("Changed connections\n");
 }
+
 void nodeTimeAdjustedCallback(int32_t offset) {
-  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
 }
 
 void setup() {
   Serial.begin(115200);
 
-  mesh.setDebugMsgTypes( ERROR | STARTUP );
+  mesh.setDebugMsgTypes(ERROR | STARTUP);
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-
-  userScheduler.addTask( taskSendMessage );
-  taskSendMessage.enable();
 }
 
 void loop() {
+  readSoilMoisture();
+  readAirIndicators();
+  sendMessage();
   mesh.update();
-
-  if (Serial.available() > 0) {
-    messageToSend = Serial.readString();
-    messageToSend.trim();
-    Serial.println("Typed message: " + messageToSend);
-  }
+  delay(1000);
 }
